262f6f1ece592c9129cb3969440f0dc940ab3f2f
Shium Hossen <112849781+SiamHosenPg@users.noreply.github.com>
2023-12-05 21:46:20 +0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function MenuContent(Contents){
+    Contents.parentElement.style.left = "-100%";
+}
+function HideShowMenu(){
+   const NaveBar =  document.querySelector('.NavSection');
+   NaveBar.style.left="0%";
+}
